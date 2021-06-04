6066c25d50788a7fb908c7314733364d2cef3115
VoidWalker <dmgwatts@gmail.com>
2021-06-04 16:55:39 -0500

    Create integrity.sh

---
+++ ansible/build/integrity.sh
@@ -0,0 +1,2 @@
+8216bcbd09024743426964ef7baa5f433d12332c57cc7a8b742bbcc33efee7df  vault_1.7.2+ent_linux_amd64.zip
+8a7f7f39c65e7eb43b11bfe4f568b44f75999e206cb1eb627bc27ab2fe04ffc7  consul_1.9.5+ent_linux_amd64.zip
